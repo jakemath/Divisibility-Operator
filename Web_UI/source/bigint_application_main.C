546e0f43d8e62b95d336ad1b7f6d0946c9c732fd
Jacob Mathai <Jacob@Jacobs-MBP.attlocal.net>
2019-08-12 21:33:53 -0700

    "Removed Web UI"

--- Web_UI/source/bigint_application_main.C
+++
@@ -1,8 +0,0 @@
-#include "bigint_application.h"
-
-int main(int argc, char **argv)
-{
-    return Wt::WRun(argc, argv, [](const Wt::WEnvironment& env) {
-        return Wt::cpp14::make_unique<BigintApplication>(env);
-    });
-}
